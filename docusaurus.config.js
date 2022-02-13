a139792511516c21ba641c1f3882da9c04909473
igor-paiva <igorbatistapaiva@outlook.com>
2022-02-13 16:23:39 -0300

    Add deploy URL to docusaurus config file
    Co-authored-by: RCristyan <rcristyan9@gmail.com>
    Co-authored-by: Rhuancpq <rhuancarlos.queiroz@gmail.com>
    Co-authored-by: thiagolopess <thiago.lopes.santos.tls@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'MeasureSoftGram',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://fga-eps-mds.github.io/2021-2-MeasureSoftGram-Doc/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
